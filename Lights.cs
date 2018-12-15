489e7ee1c0d354e7c3e28351e6731323f65fa26e
Jesse <jelloeater@gmail.com>
2018-12-14 23:48:06 -0500

    Started working on Lights class and associated methods

---
+++ Lights.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace huesharp
+{
+    internal class HttpHelper
+    {
+        public dynamic GetSiteJson(string siteIn)
+        {
+            var http = new EasyHttp.Http.HttpClient();
+            http.Request.Accept = EasyHttp.Http.HttpContentTypes.ApplicationJson;
+            var response = http.Get(siteIn);
+            return response.DynamicBody;
+        }
+    }
+
+    public class Light
+    {
+        private string name;
+        private bool state;
+
+        public Light(bool state, string name)
+        {
+            this.state = state;
+            this.name = name;
+        }
+    }
+
+    public static class Lights
+    {
+        public static List<Light> GetLightLight()
+        {
+            var lightList = new List<Light>();
+            var lightListRaw = new HttpHelper().GetSiteJson(Settings.BaseUrl).lights;
+
+            foreach (var singleLight in lightListRaw)
+            {
+                var nameParse = singleLight.Value.name;
+                var stateParse = singleLight.Value.state.on;
+                lightList.Add(new Light(name:nameParse,state:stateParse));
+            }
+            return lightList;
+        }
+    }
+}
---
+++ SettingsExample.cs
@@ -0,0 +1,10 @@
+﻿namespace huesharp
+{
+    public class SettingsExample
+//    Rename me for your own usage to "Settings"
+    {
+        private const string Ip = "127.0.0.1";
+        private const string ApiUser = "API_KEY_GOES_HERE";
+        public static string BaseUrl = "http://" + Ip + "/api/" + ApiUser;
+    }
+}
